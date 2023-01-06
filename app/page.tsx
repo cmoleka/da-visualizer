bc8f3f64a7fc0bf657e7697478b5040a5fafd8a1
carlo moleka <me@carlomoleka.com>
2023-01-06 15:25:13 -0500

    Change page type for app/page and app/ds/node/page, from NextPage to FC

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { type NextPage } from "next";
+import type { FC } from "react";
 
-const Home: NextPage = () => {
+const Home: FC = () => {
   return (
-    <section className="w-full p-2 container mx-auto">
+    <section className="container mx-auto w-full p-2">
       <header>
-        <h1 className="text-2xl font-bold">Data sctructure and Algorithms visualizer</h1>
+        <h1 className="text-2xl font-bold">
+          Data sctructure and Algorithms visualizer
+        </h1>
       </header>
       <main className="">
         <p className="text-lg">some text</p>
