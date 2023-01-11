a45d6d3ec366168f32e532b2bc5cbaeb98548391
carlo moleka <me@carlomoleka.com>
2023-01-11 16:43:10 -0500

    fixing queue state

--- app/ds/queue/page.tsx
+++ app/ds/queue/page.tsx
@@ -1,50 +1,70 @@
 "use client";
 
-import type { FC, ChangeEvent } from 'react';
+import type { FC, ChangeEvent } from "react";
 // import { Node as NodeClass } from "@lib/ds/node";
 import { Queue } from "@lib/ds/queue";
-import { useState } from 'react';
-import classNames from 'classnames';
+import { useRef, useState } from "react";
+
+interface QueueData {
+  name: string;
+}
 
 const QueuePage: FC = () => {
-  const [queueInstance, setQueueInstance] = useState<Queue | null>(null);
-  const [value, setValue] = useState<number>(0);
-  const [maxSize, setMaxSize] = useState<string>('');
-  const [allowNewQueue, setAllowNewQueue] = useState<boolean>(true);
-  const _queueInstance: Queue = new Queue(parseInt(maxSize));
+  const queueForm = useRef<HTMLFormElement>(null);
+  const [queueInstance, setQueueInstance] = useState<Queue<QueueData>>(
+    new Queue(0)
+  );
+  const [selectedVisitor, setSelectedVisitor] = useState<{ name: string }>({
+    name: "",
+  });
+  const [returnedSelectedVisitor, setReturnedSelectedVisitor] = useState<{
+    name: string;
+  }>({
+    name: "",
+  });
 
-  const VISITORS = ['Safi', 'Carlo', 'John', 'Eric'] as const;
-  const handleCreateQueue = (): void => {
-    if (!maxSize) throw Error;
-    setQueueInstance(new Queue(parseInt(maxSize)));
-    setAllowNewQueue(false);
-  };
+  const VISITORS = ["Safi", "Carlo", "John", "Eric"] as const;
+  const MAX_SIZE = [5, 10, 15, 20] as const;
 
-  const handleChangeMaxSize = ({ target }: ChangeEvent<HTMLSelectElement>): void => {
-    setMaxSize(target.value);
+  const handleChangeMaxSize = ({
+    target,
+  }: ChangeEvent<HTMLSelectElement>): void => {
+    const maxSize = target.value;
     if (!maxSize) throw Error("MaxSize is empty");
     if (!queueInstance) throw Error("The Queue is null");
-    _queueInstance.maxSize = parseInt(maxSize);
-    setQueueInstance((prevState) => {
-      if (!prevState?.maxSize) throw Error("Maxsize is null");
-      return Object.assign({}, prevState, { maxSize: _queueInstance.maxSize });
-    });
-    console.log(_queueInstance.maxSize)
+    const _queueInstance = queueInstance;
+    _queueInstance.setMaxSize.bind(_queueInstance, parseInt(maxSize));
+    setQueueInstance((prevState) => Object.assign(prevState, _queueInstance));
   };
 
-  const handleValueChange = ({ target }: ChangeEvent<HTMLInputElement>): void => {
-    setValue(parseInt(target.value));
+  const handleOnValueChange = ({
+    target,
+  }: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = target;
+    if (!name) throw Error("Name is empty");
+    if (!value) throw Error("Value is empty");
+    setSelectedVisitor({ name: value });
   };
 
-  const handleOnEnqueue = (): void => {
-    queueInstance?.enqueue(VISITORS[value] as string);
-    setValue(value + 1);
-  }
+  const handleOnEnqueue = () => {
+    if (!queueInstance) throw Error("The Queue is null");
+    queueInstance.enqueue.bind(queueInstance, selectedVisitor);
+  };
 
-  const createQueueButtonClass = classNames({
-    'bg-gray-50 text-gray-900': !allowNewQueue,
-    'bg-blue-600 text-white': allowNewQueue
-  });
+  const handleOnDequeue = (): void => {
+    if (!queueInstance) throw Error("The Queue is null");
+    const dequeuedVisitor = queueInstance?.dequeue.bind(queueInstance);
+    console.log(dequeuedVisitor);
+    if (!dequeuedVisitor) throw Error("Dequeued visitor is null");
+    setReturnedSelectedVisitor(dequeuedVisitor);
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+    if (target.buttonId == "enqueue") handleOnEnqueue();
+    if (target.buttonId == "dequeue") handleOnDequeue();
+  };
 
   return (
     <>
@@ -54,9 +74,9 @@ const QueuePage: FC = () => {
         </h1>
         <p className="text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
           A queue is an abstract data type that holds an ordered, linear
-          sequence of items. You can describe it as a first in, first out (FIFO) structure.
-          The first element to be added to the queue will be the first element to be removed
-          from the queue.
+          sequence of items. You can describe it as a first in, first out (FIFO)
+          structure. The first element to be added to the queue will be the
+          first element to be removed from the queue.
         </p>
       </header>
       <main className="flex w-full flex-col justify-between gap-4 space-y-6 divide-y-2">
@@ -64,34 +84,84 @@ const QueuePage: FC = () => {
           <h2 className="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-xl lg:text-2xl">
             Mutations
           </h2>
-          <div className='flex flex-col md:flex-row w-full space-y-4 md:space-y-0'>
-            <label htmlFor="maxsize" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your country</label>
-            <select name="maxSize" id="countries" defaultValue="Choose Here" onChange={(e) => handleChangeMaxSize(e)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option>5</option>
-              <option>10</option>
-              <option>15</option>
-              <option>20</option>
-            </select>
-            <button
-              value="submit"
-              className={`${createQueueButtonClass}  text-base w-full  rounded-md py-2 px-4 font-bold capitalize`}
-              onClick={() => handleCreateQueue()}
+          <div className="flex w-full flex-col space-y-4 md:flex-row md:space-y-0">
+            <label
+              htmlFor="maxsize"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
             >
-              {allowNewQueue ? "Create new Queue" : "Queue created"}
-            </button>
+              Select your country
+            </label>
+            <select
+              name="maxSize"
+              id="countries"
+              defaultValue="Choose Here"
+              onChange={(e) => handleChangeMaxSize(e)}
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            >
+              {MAX_SIZE.map((size, index) => (
+                <option key={index} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>
           </div>
-          <input
-            type="text"
-            onChange={handleValueChange}
-            placeholder="Enter data"
-            className="block w-full rounded-lg border border-gray-300 bg-blue-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          />
+          <form
+            onSubmit={handleOnSubmit}
+            className="flex flex-col space-y-4"
+            ref={queueForm}
+          >
+            {VISITORS.map((visitor, index) => (
+              <div className="flex flex-row space-x-4" key={index}>
+                <input
+                  onChange={(e) => handleOnValueChange(e)}
+                  type="radio"
+                  name="visitor"
+                  value={visitor}
+                  id={visitor}
+                />
+                <label htmlFor={visitor}>{visitor}</label>
+              </div>
+            ))}
+            <button
+              id="enqueue"
+              type="submit"
+              name="enqueue"
+              className="w-full rounded-md  bg-blue-600 py-2  px-4 text-base font-bold capitalize text-white"
+              onClick={(e) => {
+                if (!queueForm.current) throw Error("QueueForm is null");
+                const target = e.target as HTMLButtonElement;
+                queueForm.current.buttonId = target.name;
+              }}
+            >
+              Add to Queue
+            </button>
+            <button
+              id="dequeue"
+              type="submit"
+              name="dequeue"
+              className="w-full rounded-md  bg-red-600 py-2  px-4 text-base font-bold capitalize text-white"
+              onClick={(e) => {
+                if (!queueForm.current) throw Error("QueueForm is null");
+                const target = e.target as HTMLButtonElement;
+                queueForm.current.buttonId = target.name;
+              }}
+            >
+              Remove from Queue
+            </button>
+          </form>
           <div className="flex flex-row flex-wrap justify-center space-y-2 space-x-4 divide-x-2">
+            {queueInstance?.getQueue().map((visitor, index) => (
+              <div key={index} className="flex flex-col space-y-2">
+                <span className="text-sm font-bold text-gray-900 dark:text-white">
+                  {visitor.name}
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
 export default QueuePage;
--- lib/ds/queue.ts
+++ lib/ds/queue.ts
@@ -1,23 +1,23 @@
-export class Queue {
+export class Queue<T> {
   // A queue is an abstract data type that holds an ordered, linear
   // sequence of items. You can describe it as a first in, first out (FIFO) structure.
   // The first element to be added to the queue will be the first element to be removed
   // from the queue.
-  private queue: object[];
+  private queue: T[];
   public maxSize: number;
   private front: number;
   private rear: number;
 
   constructor(maxSize: number) {
-    this.queue = []
-    this.maxSize = maxSize
-    this.front = 0
-    this.rear = -1
-  };
+    this.queue = [];
+    this.maxSize = maxSize;
+    this.front = 0;
+    this.rear = -1;
+  }
 
   // getQueue: Returns {list} of Queue.
-  getQueue(): object[] {
-    return this.queue
+  getQueue(): T[] {
+    return this.queue;
   }
 
   // setMaxSize: Set the maxSize {number} of the queue.
@@ -38,19 +38,21 @@ export class Queue {
   }
 
   // enqueue: Inserts {item} inside the queue.
-  enqueue(data: {} | null): void {
+  enqueue(data: T): void {
     if (this.isFull()) return;
-    this.rear = this.rear + 1
+    this.rear = this.rear + 1;
     if (!data) throw Error;
-    this.queue.push(data)
+    console.log(data);
+    this.queue.splice(this.rear, 0, data);
   }
 
   // dequeuek: Removes {item} from the queue and return it.
-  dequeue() {
-    let dequeuedItem = null;
-    if (this.isEmpty()) return;
-    dequeuedItem = this.queue.pop()
-    this.front = this.front + 1
-    return dequeuedItem
+  dequeue(): T {
+    let dequeuedItem: T | null = null;
+    if (this.isEmpty()) throw Error("Queue is empty");
+    dequeuedItem = this.queue.shift() as T;
+    console.log(dequeuedItem);
+    this.front = this.front + 1;
+    return dequeuedItem;
   }
 }
